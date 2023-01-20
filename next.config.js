1465dc11a7e2563ffae0f236d6e70e2241481d35
Shuhaib-Ahamed <shuhaibsamadh@gmail.com>
2023-01-20 19:25:41 +0530

    added starter template

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["twitter.com", "pbs.twimg.com"],
+  },
 };
 
 module.exports = nextConfig;
