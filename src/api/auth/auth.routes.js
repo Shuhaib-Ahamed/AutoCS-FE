fc11a66475534141338ff8603f4a70fae21d0996
Shuhaib-Ahamed <shuhaibsamadh@gmail.com>
2023-01-20 09:16:14 +0530

    added starter template

--- src/api/auth/auth.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import controller from "./auth.controller.js";
-
-const router = express.Router();
-router.post("/login", controller.login);
-router.post("/account/create", controller.createAccount);
-export default router;
--- src/utils/networks.js
+++
@@ -1,4 +0,0 @@
-export const NETWORKS = {
-  PUBLIC: "Public Global Stellar Network ; September 2015",
-  TESTNET: "Test SDF Network ; September 2015",
-};
